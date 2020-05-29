d0663bc4565a67f9f9e1be3e5d05412bdb8269f6
Qurat Ul Ain Shoro <65081454+QuratAin@users.noreply.github.com>
2020-05-29 03:03:54 -0700

    Add files via upload

---
+++ qurat.java
@@ -0,0 +1,14 @@
+//By extending Frame Class
+import java.awt.Frame;
+class Frame1 extends Frame {
+	public Frame1() {
+	setSize(500,300);
+	setVisible(true);
+	setTitle("Qurat Ul Ain");
+}
+}
+public class qurat{
+public static void main(String args[]) {
+	Frame1 obj = new Frame1 ();
+}
+}
